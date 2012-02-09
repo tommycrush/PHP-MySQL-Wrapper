86361ec9c99ca58dd0b418bf0605ed08a8088f68
tcrush0808 <thomas.crush@uky.edu>
2012-02-09 16:06:22 -0500

    Update examples

--- index.php
+++ index.php
@@ -6,31 +6,6 @@ ini_set("display_errors",1);
 require("MySQL.class.php");
 $db = new MySQL_Wrapper("localhost", "root", "root", "test_database");
 
-
-
-
-$id = $db->insertAndReturnID("INSERT INTO `users` (`email`,`first_name`,`last_name`) VALUES ('email','fist','name') ");
-
-
-$id = $db->escape($id);
-
-$test  = $db->getOneRow("SELECT * FROM `users` WHERE user_id='$id' LIMIT 1");
-echo "<br/><pre>";
-print_r($test);
-echo "</pre><br/>";
-
-foreach($db->getMultipleRows("SELECT * FROM `users`") as $row){
-	echo $row["user_id"]."<br/>";
-}
-
-
-echo "loaded $id";
-
-$test = $db->smartInsert("users", array('email','first_name','last_name'), array(array('me@tommy.com','firstest','best'),array('2nd email','blah','crush')));
-echo $test;
-
-
-
 /*
 //connection example for MySQLi
 require('MySQLi.class.php');
@@ -52,39 +27,47 @@ $db = new MySQLi_Wrapper("localhost", "root", "root", "test_database");
  * 
  * 		MORE FUNCTIONS:
  * 
- * 		getOneRow($query)
+ * 		getOneRow(string $query)
  * 			returns an array of the data, or false if anything but 1 was returned by the query
  * 			note, use LIMIT 1 in the query
  * 	
- * 		getMultipleRows($query)
+ * 		getMultipleRows(string $query)
  * 			returns a 2D array of the data, or false is nothing was returned by the query
  * 
- * 		insertAndReturnID($query)
+ * 		insertAndReturnID(string $query)
  * 			returns the insert_id
  * 
+ * 		smartInsert(string $table_name, array $columns, array $values); 1 row insert
+ * 			returns the insert_id
+ * 		smartInsert(string $table_name, array $columns, array array $values); multi row insert
+ * 			return true;
  */
 
- /*
-//insert a new row
-$insert_id = $db->insertAndReturnID("INSERT INTO `test` (`col1`,`col2`) VALUES ('v1', 'v2')");
-
-//OR, lets insert mutiple rows using the raw "query" function
-$db->query("INSERT INTO `test` (`col1`,`col2`) VALUES ('v2', 'v2'),('v3','v3')");
+ 
+ //insert and return ID
+$new_id = $db->insertAndReturnID("INSERT INTO `users` (`email`,`first_name`,`last_name`) VALUES ('email','fist','name') ");
 
 
 
-//get one row of data
-$data = $db->getOneRow("SELECT * FROM `test` WHERE `col1`='v1' LIMIT 1");
+//escape data
+$id = $db->escape('tommy');
 
-//get a 2d array of the data
-foreach($db->getMultipleRows("SELECT * FROM `test`") as $row){
-	echo $row["col1"]."<br/>";
+
+
+//if we only want 1 row of data, we can get it returned as an array to simplify things
+$data  = $db->getOneRow("SELECT * FROM `users` WHERE first_name='$id' LIMIT 1");
+
+//lets see what we got:
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+//lets get a lot of data returned to us in a 2D array
+foreach($db->getMultipleRows("SELECT `first_name` FROM `users`") as $row){
+	echo $row["first_name"]."<br/>";
 }
-  * 
-  */
 
 
-
-
+$success = $db->smartInsert("users", array('email','first_name','last_name'), array(array('me@tommycrush.com','tommy','crush'),array('you@you.com','John','Doe')));
 
 ?>
